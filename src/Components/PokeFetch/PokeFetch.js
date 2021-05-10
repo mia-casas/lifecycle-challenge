bf9451494eaa32cb7aa4f3fc7dbd67d3bad667dc
Mia Casas <miacasas@mias-mbp.attlocal.net>
2021-05-09 22:06:02 -0500

    challenge

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -9,10 +9,16 @@ class PokeFetch extends Component {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      timer: 10,
+      timerRunning: false
     }
+    this.timer = this.timer.bind(this);
   }
 
   fetchPokemon() {
+    this.setState(
+      {timerRunning: true}
+    )
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
@@ -25,8 +31,25 @@ class PokeFetch extends Component {
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
         })
+        this.timer()
       })
       .catch((err) => console.log(err))
+  }
+
+  timer(){
+    if(this.state.timer> 0){
+      this.setState({timer: this.state.timer -1})
+    } if(this.state.timer === 0) {
+      clearInterval(this.state.timer)
+      this.setState(
+        {timer: 10, timerRunning:false}
+      )
+    }
+    console.log(this.state.timer)
+  }
+
+  componentDidUpdate(){
+    setInterval(() => {this.timer()}, 1000)
   }
 
   render() {
@@ -34,9 +57,10 @@ class PokeFetch extends Component {
       <div className={'wrapper'}>
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
         <h1 className={'timer'} >Timer Display</h1>
+        <h3>{this.state.timerRunning ? this.state.timer : null}</h3>
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+          <img className={this.state.timerRunning ? 'pokeAltImg' : 'pokeImg'} src={this.state.pokeSprite} />
+          <h1 className={'pokeName'}>{this.state.timerRunning ? null : this.state.pokeName}</h1>
         </div>
       </div>
     )
